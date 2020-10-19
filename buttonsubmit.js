e2f3fc6f33504ee26ddc75230659398af7731e81
hollyyph <hollyana.puteri.haryono@gmail.com>
2020-10-19 21:49:58 +0700

    balik

---
+++ buttonsubmit.js
@@ -0,0 +1,8 @@
+function submitform(firstname, lastname) {
+    alert("Success writing name. \nWelcome " + firstname + " " + lastname + "!");
+}
+
+var fname = "mama";
+var lname = "papa";
+
+submitform(fname, lname);
