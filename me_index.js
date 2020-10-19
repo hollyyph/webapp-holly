610b66b54cc79533835db149136f7f4b3e5463cc
hollyyph <hollyana.puteri.haryono@gmail.com>
2020-10-19 09:25:38 +0700

    mirip quickstart

--- me_index.js
+++ me_index.js
@@ -1,15 +1,3 @@
-// const http = require('http');
-
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-// });
-
-// const port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
-
 const express = require("express")
 const path = require('path')
 const app = express()
